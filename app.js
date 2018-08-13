d46642cb77cedd8d11257a79be783b9239ae8647
Milek Agrawal <milekag02@gmail.com>
2018-08-13 21:43:32 +0000

    initial commit

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.get('/products',function(req,res){
     res.render('index');
 });
 
-app.listen(8000,function(){
+app.listen(process.env.PORT,process.env.IP,function(){
     console.log('Server started');
 });
