b046d381b2c26f24e223d574b6720bbc13c46a84
Milek Agrawal <milekag02@gmail.com>
2018-08-16 21:55:37 +0000

    seed data added,signin added,signup added

--- routes/routes.js
+++ routes/routes.js
@@ -40,5 +40,14 @@ router.post('/user/signup',passport.authenticate('local.signup',{
 router.get('/user/profile',function(req,res,next){
     res.render('user/profile');
 });
+router.get('/user/signin',csrfProtection,function(req,res){
+    var messages = req.flash('error');
+    res.render("user/login",{csrfToken : req.csrfToken(), messages: messages,hasErrors: messages.length>0});
+});
+router.post('/user/signin',passport.authenticate('local.signin',{
+    successRedirect: '/user/profile',
+    failureRedirect: '/user/signin',
+    failureFlash: true
+}));
 
 module.exports = router;
