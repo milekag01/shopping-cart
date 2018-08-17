b56ef049ac5e0fe70de5369f967614d84af13a05
Milek Agrawal <milekag02@gmail.com>
2018-08-17 15:30:36 +0000

    cart database added

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 var Product = require("../models/product");
+var Cart = require("../models/cart");
 
 router.get('/',function(req,res){
     res.redirect('/products');
@@ -20,6 +21,21 @@ router.get('/products',function(req,res){
                 }
                 res.render('index',{product:productchunks});
             }      
+    });
+});
+
+router.get('/add-to-cart/:id',function(req,res,next){
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : {});
+    
+    Product.findById(productId,function(err,product){
+        if(err){
+            return res.redirect('/');
+        }
+        cart.add(product,product.id);
+        req.session.cart = cart;
+        console.log(req.session.cart);
+        res.redirect('/');
     });
 });
 
