9770c7932135dc6f39687dde5280740b03f96d83
Milek Agrawal <milekag02@gmail.com>
2018-08-18 00:55:49 +0000

    checkout page added

--- routes/routes.js
+++ routes/routes.js
@@ -47,4 +47,12 @@ router.get('/shopping-cart',function(req,res,next){
     res.render('cart',{products: cart.generateArray() , totalPrice: cart.totalPrice});
 });
 
+router.get('/checkout',function(req,res,next){
+    if(!req.session.cart){
+        return res.render('/shopping-cart');
+    }
+    var cart = new Cart(req.session.cart);
+    res.render('checkout',{total: cart.totalPrice});
+});
+
 module.exports = router;
